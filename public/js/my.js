cd18e030d771bf0f7e3d095bc7ac093ade3149fb
Chema <didelco@gmail.com>
2015-02-08 19:41:05 +0100

    hello sliders & jquery
    sliders added & jquery & jquery UI

---
+++ public/js/my.js
@@ -0,0 +1,11 @@
+$(function() {
+  $( "#eq > span" ).each(function() {
+    var value = parseInt( $( this ).text(), 10 );
+    $(this).empty().slider({
+      value: value,
+      range: "min",
+      animate: true,
+      orientation: "vertical"
+    });
+  });
+});
